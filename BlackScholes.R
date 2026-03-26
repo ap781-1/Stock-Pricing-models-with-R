c17505ef605df1f7ce9cfda5fec9c658e9587f9a
Atharva Patkar <atharvaapatkar@gmail.com>
2026-03-26 18:12:34 +0100

    Refactor Black-Scholes function and update parameters

--- BlackScholes.R
+++ BlackScholes.R
@@ -1,15 +1,18 @@
 #Black Scholes Formula and Replications
-#S0 - Stock Price, X- Exercise Price, r- Risk free rate, T- Time of expiration, sigma- sd of log returns
+#S0 - Stock Price, K- Exercise Price, r- Risk free rate, T- Time of expiration, sigma- sd of log returns
 # Call option for European: C0 = S0*N(d1) - Xe^(-rT)N(d2)
 #d1 = [ln(S0/X)+(r+(\sigma²/2)]/\sigma\root T
 #d2 = [ln(S0/X)+(r-(\sigma²/2)]/\sigma\root T
 
-BlackScholes_base <- function(S, X, r, T, sig){
-  d1 <- (log(S/X) + (r + sig^2/2)*T) / (sig * sqrt(T))
-  d2 <- (log(S/X) + (r - sig^2/2)*T) / (sig * sqrt(T))
-  cbs <- S * pnorm(d1) - X * exp(-r*T) * pnorm(d2)
-  return(cbs)
+# Black-Scholes formula 
+black_scholes <- function(S0, K, T, r, sigma) {
+  d1 <- (log(S0/K) + (r + sigma^2/2) * T) / (sigma * sqrt(T))
+  d2 <- d1 - sigma * sqrt(T)
+    price <- S0 * pnorm(d1) - K * exp(-r * T) * pnorm(d2)
+  return(price)
 }
 
-call <- BlackScholes_base(S=102, X=100, r=0.02, T=0.5, sig=0.2)
+
+call <- black_scholes(S0=102, K=100, r=0.02, T=0.5, sigma=0.2)
 call
+
